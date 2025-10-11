57d4a8639448fea8a125a01fd403f478fe771621
NotKeKe <evamark0130@gmail.com>
2025-10-11 23:08:50 +0800

    搜尋工具優化: 基於 asyncio.task 優化整體運行速度

--- cmds/ai_chat/tools/func/web_search.py
+++ cmds/ai_chat/tools/func/web_search.py
@@ -1,15 +1,15 @@
-from aiohttp import ClientSession
-import orjson
+from aiohttp import ClientSession, TCPConnector
+import asyncio
 from bs4 import BeautifulSoup
 
 from core.functions import DEVICE_IP
 
 NOT_AVAILABLE = 'web_search is not available'
 
+searching_sem = asyncio.Semaphore(40)
+headers = {"User-Agent": 'Mozilla/5.0'}
+
 async def web_search(keywords: str, time_range: str = 'year', language: str = 'zh-TW') -> str:
-    # 先查資料庫中有沒有對應的資料 再進行搜尋
-    # sql_data = knowledge_base_search(keywords)
-    # result = [sql_data] if sql_data else []
     result = []
     time_range = ('day' if time_range.lower().strip() not in ('year', 'monuth', 'week', 'day') else time_range.lower().strip()) if time_range else None
 
@@ -36,34 +36,34 @@ async def web_search(keywords: str, time_range: str = 'year', language: str = 'z
             
     urls = [item['url'] for item in data['results']][:10]
 
-    async def scrape_page(url) -> str:
-        headers = {"User-Agent": 'Mozilla/5.0'}
-
-        async with ClientSession() as session:
-            async with session.get(url, headers=headers, timeout=5) as resp:
-                if resp.status != 200: return 'None', 'None'
-                text = await resp.text()
-
-        soup = BeautifulSoup(text, "html.parser")
-        titleObj = soup.find("title")
-
-        if titleObj is None: return None, None
-
-        title = titleObj.text
-        paragraphs = [p.text for p in soup.find_all("p")]
-        content = "\n".join(paragraphs)
-
-        return title, content
-
-    
-    for url in urls:
+    async def scrape_page(session: ClientSession, url) -> tuple[str, str, str]:
         try:
-            if url.endswith('.pdf'): continue
-            title, content = await scrape_page(url)
-            if title is None and content is None: continue
-            result.append(f"標題: {title}\n內容: {content[:400]}...\n連結: {url}\n")  # 只顯示前200字
+            async with searching_sem:
+                async with session.get(url, headers=headers, timeout=5) as resp:
+                    if resp.status != 200: return None, None, None
+                    text = await resp.text()
+
+                soup = BeautifulSoup(text, "html.parser")
+                titleObj = soup.find("title")
+
+                if titleObj is None: return None, None, None
+
+                title = titleObj.text
+                paragraphs = [p.text for p in soup.find_all("p")]
+                content = "\n".join(paragraphs)
+
+                return title, content, url
         except Exception as e:
             print(f"爬取失敗: {url}, 錯誤: {e}")
+            return None, None, None
+
+    async with ClientSession() as session:
+        tasks = [scrape_page(session, url) for url in urls if not url.endswith('.pdf')]
+        task_results = await asyncio.gather(*tasks)
+
+    for title, content, url in task_results:
+        if title is None and content is None: continue
+        result.append(f"標題: {title}\n內容: {content[:400]}...\n連結: {url}\n\n")  # 只顯示前200字
 
     if not result: return "web_search didn't find any answer."
 
