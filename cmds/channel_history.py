47c7c86b5034c98505a107410e0c3f1819d04728
NotKeKe <evamark0130@gmail.com>
2025-11-13 19:20:39 +0800

    Bug修復: 修復 output_chat_history 的 KeyError，順便優化一下代碼
    我之前在寫什麼:)
    用 aiofiles 去存 然後又把 meta data 直接放第一項
    反正我改成用 BytesIO 了

--- cmds/channel_history.py
+++ cmds/channel_history.py
@@ -1,11 +1,12 @@
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
-import aiofiles
 import orjson
 import os
 import time
 import traceback
+from textwrap import dedent
+import io
 
 from core.classes import Cog_Extension
 from core.translator import locale_str, load_translated
@@ -20,23 +21,23 @@ class ChannelHistories:
         self.reverse = reverse
         self.ls = None
         self.channelID = ctx.channel.id
-        self.file_path = None
+        self.file: discord.File | None = None
 
     async def get_histories(self) -> list:
         ctx = self.ctx
         count = self.count
         ls = [{
-            'channel': {'id': ctx.channel.id, 'name': ctx.channel.name}, 
+            'channel': {'id': ctx.channel.id, 'name': ctx.channel.name if hasattr(ctx.channel, 'name') else None}, # type: ignore
             **({'guild': {'id': ctx.guild.id, 'name': ctx.guild.name}} if ctx.guild else {})
         }]
         async for m in ctx.channel.history(limit=count):
             if m.content == m.attachments == m.embeds == None: continue
 
-            ls.append({
+            ls.append({ # type: ignore
                 'author': {
                     'id': m.author.id,
                     'name': m.author.global_name or m.author.name,
-                    'avatar_url': m.author.avatar.url
+                    'avatar_url': m.author.avatar.url if m.author.avatar else None
                 },
                 'content': m.content,
                 'timestamp': m.created_at.strftime('%Y-%m-%d %H:%M:%S'),
@@ -57,32 +58,46 @@ class ChannelHistories:
         return ls
     
     async def type_process(self):
+        if not self.ls: return
         if self.reverse:
             self.ls.reverse()
 
         if self.file_type == 'json':
-            self.file_path = f'{PATH}/channel_history_{self.channelID}.json'
-            async with aiofiles.open(f'{PATH}/channel_history_{self.channelID}.json', mode='w') as f:
-                await f.write(orjson.dumps(self.ls, option=orjson.OPT_INDENT_2).decode())
+            self.file = discord.File(
+                io.BytesIO(
+                    orjson.dumps(self.ls, option=orjson.OPT_INDENT_2)
+                ), 
+                filename=f'channel_history_{self.channelID}.json'
+            )
         else:
-            self.file_path = f'{PATH}/channel_history_{self.channelID}.txt'
             result = []
-            for m in self.ls:
-                i18n_data = await self.ctx.interaction.translate('send_channel_history_file_template')
-                i18n_data = load_translated(i18n_data)[0]
-                
-                author_str = i18n_data.get('author', '作者')
-                avatar_url_str = i18n_data.get('avatar_url', '頭像連結')
-                time_str = i18n_data.get('time', '時間')
-                content_str = i18n_data.get('content', '內容')
-                none_str = i18n_data.get('none', '無')
-                attachments_str = i18n_data.get('attachments', '附件')
-                embed_str = i18n_data.get('embed', '嵌入訊息 (embed)')
-                embed_title_str = i18n_data.get('embed_title', '嵌入標題')
-                embed_description_str = i18n_data.get('embed_description', '嵌入描述')
-                embed_fields_str = i18n_data.get('embed_fields', '嵌入欄位')
-                embed_image_str = i18n_data.get('embed_image', '嵌入圖片')
 
+            i18n_data = await self.ctx.interaction.translate('send_channel_history_file_template')
+            i18n_data = load_translated(i18n_data)[0]
+            
+            author_str = i18n_data.get('author', '作者')
+            avatar_url_str = i18n_data.get('avatar_url', '頭像連結')
+            time_str = i18n_data.get('time', '時間')
+            content_str = i18n_data.get('content', '內容')
+            none_str = i18n_data.get('none', '無')
+            attachments_str = i18n_data.get('attachments', '附件')
+            embed_str = i18n_data.get('embed', '嵌入訊息 (embed)')
+            embed_title_str = i18n_data.get('embed_title', '嵌入標題')
+            embed_description_str = i18n_data.get('embed_description', '嵌入描述')
+            embed_fields_str = i18n_data.get('embed_fields', '嵌入欄位')
+            embed_image_str = i18n_data.get('embed_image', '嵌入圖片')
+
+            # 例外
+            message_str = dedent(
+                f'''
+                Guild: {self.ctx.guild.name if self.ctx.guild else none_str} - {self.ctx.guild.id if self.ctx.guild else none_str}
+                Channel: {self.ctx.channel.name if hasattr(self.ctx.channel, 'name') else none_str} - {self.ctx.channel.id}
+                '''
+            ).strip() + '\n'
+            message_str += "-" * 30 + "\n"
+            result.append(message_str)
+
+            for m in self.ls[1:]:
                 message_str = f"{author_str}: {m['author']['name']} ({m['author']['id']})\n" \
                               f"{avatar_url_str}: {m['author']['avatar_url'] or none_str}\n" \
                               f"{time_str}: {m['timestamp']}\n" \
@@ -91,7 +106,7 @@ class ChannelHistories:
                 if m['attachments'] is not None:
                     message_str += f"{attachments_str}: {', '.join(m['attachments'])}\n"
                 
-                if m['embeds'] is not None:
+                if m['embeds'] is not None and m['embeds']:
                     for embed in m['embeds']:
                         message_str += f"{embed_str}:\n"
                         if embed['title'] is not None and embed['title'] != none_str:
@@ -108,14 +123,18 @@ class ChannelHistories:
                 message_str += "-" * 30 + "\n"
                 result.append(message_str)
 
-            async with aiofiles.open(f'{PATH}/channel_history_{self.channelID}.txt', mode='w', encoding='utf-8') as f:
-                await f.write("".join(result))
+            self.file = discord.File(
+                io.BytesIO(
+                    ''.join(result).encode('utf-8'), 
+                ),
+                filename=f'channel_history_{self.channelID}.txt'
+            )
 
-    async def run(self):
+    async def run(self) -> discord.File:
         try:
             await self.get_histories()
             await self.type_process()
-            return self.file_path
+            return self.file
         except: traceback.print_exc()
 
 class ChannelHistory(Cog_Extension):
@@ -136,9 +155,10 @@ class ChannelHistory(Cog_Extension):
                 return await ctx.send(await ctx.interaction.translate('send_output_chat_history_bot_no_read_permission'))
 
             ch = ChannelHistories(ctx, count, file_type, reverse)
-            file_path = await ch.run()
-            
-            file = discord.File(file_path, f'channel_history_{ctx.channel.id}.{file_type}')
+            file = await ch.run()
+
+            if file is None:
+                raise FileNotFoundError('檔案不存在 in output_chat_history')
 
             await ctx.send(file=file)
 
