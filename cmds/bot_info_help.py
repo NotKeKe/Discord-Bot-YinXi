d5e05143e6ecc44a0b344f19449be2d275e36af9
NotKeKe <149957866+NotKeKe@users.noreply.github.com>
2026-05-13 20:13:14 +0800

    刪除未使用的class，並完成 type error 修復

--- cmds/bot_info_help.py
+++ cmds/bot_info_help.py
@@ -3,53 +3,12 @@ from discord import app_commands
 from discord.app_commands import Choice
 from discord.ext import commands
 from datetime import datetime
-import os
-import json
-from dotenv import load_dotenv
-import typing
+from typing import Optional
 import traceback
 
 from core.classes import Cog_Extension, bot
 from core.functions import create_basic_embed
 from core.translator import locale_str, load_translated, get_translate
-
-# get env
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-KeJC_ID = int(os.getenv('KeJC_ID'))
-
-with open('setting.json', "r") as f:
-    jdata = json.load(f)
-
-class CommandSelectView(discord.ui.View):
-    def __init__(self, bot: commands.Bot, cogname: str):
-        super().__init__(timeout=300)
-        self.bot = bot
-        self.cogname = cogname
-        if not cogname: return
-        options = [discord.SelectOption(label=command.name) for command in bot.get_cog(cogname).get_commands()]
-        self.children[0].options = options[:25]
-
-    @discord.ui.select(placeholder=locale_str('select_bot_info_help_command_placeholder'), min_values=1, max_values=1)
-    async def select_callback(self, interaction: discord.Interaction, select: discord.ui.Select):
-        try:
-            await interaction.response.defer()
-            
-            '''i18n'''
-            no_desc_str = await get_translate('send_bot_info_help_command_no_description', interaction)
-            error_str = await get_translate('send_bot_info_help_view_error', interaction)
-            ''''''
-            
-            value = select.values[0]
-
-            cmd = self.bot.get_command(value)
-            docstring = cmd.callback.__doc__ or cmd.description or no_desc_str
-
-            embed=discord.Embed(title=value, description=docstring, color=interaction.user.color, timestamp=datetime.now())
-
-            await interaction.message.edit(embed=embed, view=self)
-        except Exception as e:
-            await interaction.response.send_message(content=error_str.format(e=e), ephemeral=True)
 
 class CogSelectView(discord.ui.View):
     def __init__(self, bot: commands.Bot):
@@ -62,11 +21,11 @@ class CogSelectView(discord.ui.View):
         self.cogname = None
 
     @discord.ui.select(
-            placeholder = locale_str('select_bot_info_help_cog_placeholder'), 
-            min_values=1, 
-            max_values=1
-            )
-    async def select_callback(self, interaction: discord.Interaction, select: discord.ui.Select) :
+        placeholder = locale_str('select_bot_info_help_cog_placeholder'), 
+        min_values=1, 
+        max_values=1
+    )
+    async def select_callback(self, interaction: discord.Interaction, select: discord.ui.Select):
         try:
             '''i18n'''
             cog_no_commands_str = await get_translate('send_bot_info_help_cog_no_commands', interaction)
@@ -82,12 +41,14 @@ class CogSelectView(discord.ui.View):
             self.cogname = value
             
             # 取得該項類別中的指令名稱
-            if not self.bot.get_cog(value).get_commands(): await interaction.response.send_message(cog_no_commands_str, ephemeral=True); return
-            commands_list = [command.name for command in self.bot.get_cog(value).get_commands()]
+            cog = self.bot.get_cog(value)
+            if not cog: return
+            if not cog.get_commands(): await interaction.response.send_message(cog_no_commands_str, ephemeral=True); return
+            commands_list = [command.name for command in cog.get_commands()]
             commands_list.sort()
 
             # 更改Select
-            self.children[0].disabled = True
+            self.children[0].disabled = True # type: ignore
 
             # Embed
             embed = discord.Embed(title=embed_title_str, color=discord.Color.blue(), timestamp=datetime.now())
@@ -95,16 +56,18 @@ class CogSelectView(discord.ui.View):
                 embed.add_field(name=f'**{command}**', value=' ', inline=True)
             
             if len(commands_list) > 25:
-                await interaction.message.reply(more_commands_str.format(value=value))
+                if interaction.message:
+                    await interaction.message.reply(more_commands_str.format(value=value))
                 
             # Send Embed and stop self
-            await interaction.message.edit(embed=embed, view=self)
+            if interaction.message:
+                await interaction.message.edit(embed=embed, view=self)
             await interaction.response.defer()
             self.stop()
         except Exception as e:
             await interaction.response.send_message(content=error_str.format(e=e), ephemeral=True)
 
-async def cogName_autocomplete(interaction: discord.Interaction, current: str) -> typing.List[Choice[str]]:
+async def cogName_autocomplete(interaction: discord.Interaction, current: str) -> list[Choice[str]]:
     try:
         cogs = list(bot.cogs.keys())
 
@@ -115,13 +78,17 @@ async def cogName_autocomplete(interaction: discord.Interaction, current: str) -
 
         # 限制最多回傳 25 個結果
         return [Choice(name=c, value=c) for c in cogs[:25]]
-    except: traceback.print_exc()
+    except: 
+        traceback.print_exc()
+        return []
     
-async def cmdName_autocomplete(interaction: discord.Interaction, current: str) -> typing.List[Choice[str]]:
+async def cmdName_autocomplete(interaction: discord.Interaction, current: str) -> list[Choice[str]]:
     cogName = interaction.namespace.cog_name
 
     if cogName:
-        cmds = bot.get_cog(cogName).get_commands()
+        cog = bot.get_cog(cogName)
+        if not cog: return []
+        cmds = cog.get_commands()
     else:
         cmds = bot.commands
         
@@ -135,7 +102,7 @@ async def cmdName_autocomplete(interaction: discord.Interaction, current: str) -
     return [Choice(name=c, value=c) for c in cmds[:25]]
 
 
-class Bot_Info_and_Help(Cog_Extension):
+class BotInfoAndHelp(Cog_Extension):
     
     @commands.Cog.listener()
     async def on_ready(self):
@@ -168,32 +135,15 @@ class Bot_Info_and_Help(Cog_Extension):
             async def button_callback(interaction: discord.Interaction):
                 await ctx.invoke(self.bot.get_command('help'))
                 await interaction.response.defer()
-            button.callback = button_callback
+            button.callback = button_callback # type: ignore
             view.add_item(button)
 
             await ctx.send(embed=embed, view=view)
 
-    # Commands help
-    # @commands.hybrid_command(aliases=['helping'], name="指令幫助", description="Commands help")
-    # async def choose(self, ctx:commands.Context):
-    #     '''為什麼你需要幫助:thinking:'''
-    #     try:
-    #         # 第一個 View (讓使用者選擇cog)
-    #         view = CogSelectView(self.bot)
-    #         message = await ctx.send("Your option", view=view)
-    #         await view.wait()
-    #         # 如果cog name bug的話就return
-    #         # if view.cogname is None: await ctx.send('你選的東西呢:thinking:', ephemeral=True); return
-    #         # 第二個 View (在使用者選擇完cog後 裡面的指令們)
-    #         view2 = CommandSelectView(self.bot, view.cogname)
-    #         await message.edit(view=view2)
-    #     except Exception as exception:
-    #         await ctx.invoke(self.bot.get_command('errorresponse'), 檔案名稱=__name__, 指令名稱=ctx.command.name, exception=exception, user_send=False, ephemeral=False)
-
     @commands.hybrid_command(name=locale_str('help'), description=locale_str('help'), aliases=['helping'])
     @app_commands.describe(cog_name=locale_str('help_cog_name'), cmd_name=locale_str('help_cmd_name'))
     @app_commands.autocomplete(cog_name=cogName_autocomplete, cmd_name=cmdName_autocomplete)
-    async def help_test(self, ctx: commands.Context, cog_name: str = None, cmd_name: str = None):
+    async def help_test(self, ctx: commands.Context, cog_name: Optional[str] = None, cmd_name: Optional[str] = None):
         async with ctx.typing():
             '''i18n'''
             no_desc_str = await get_translate('send_bot_info_help_command_no_description', ctx)
@@ -211,10 +161,18 @@ class Bot_Info_and_Help(Cog_Extension):
 
             if cmd_name:
                 cmd = self.bot.get_command(cmd_name)
+                if not cmd:
+                    return await ctx.send(no_desc_str, ephemeral=True)
+
                 docstring = cmd.callback.__doc__ or cmd.description or no_desc_str
                 embed = discord.Embed(title=f'{cmd_name} ({cmd.cog_name})', description=docstring, color=ctx.author.color, timestamp=datetime.now())
             else:
-                cmds = self.bot.get_cog(cog_name).get_commands()
+                if not cog_name:
+                    return await ctx.send(no_desc_str, ephemeral=True)
+                cog = self.bot.get_cog(cog_name)
+                if not cog:
+                    return await ctx.send(no_desc_str, ephemeral=True)
+                cmds = cog.get_commands()
                 total_cmds = len(cmds)
                 
                 '''i18n'''
@@ -235,4 +193,4 @@ class Bot_Info_and_Help(Cog_Extension):
 
 
 async def setup(bot):
-    await bot.add_cog(Bot_Info_and_Help(bot))
+    await bot.add_cog(BotInfoAndHelp(bot))
