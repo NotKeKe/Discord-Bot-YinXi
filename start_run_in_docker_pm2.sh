412542c947664aa119aa9f5c3532a5057940ca4f
NotKeKe <149957866+NotKeKe@users.noreply.github.com>
2026-05-19 18:20:03 +0800

    刪除了一些不必要的檔案，以及cmds/useless/
    這些檔案現在可以在 archive branch 查看

--- start_run_in_docker_pm2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pm2 start newbot2.py --name "DiscordBot" --interpreter python3 \
-# pm2 start newbot2.py --name "DiscordBot" --interpreter python3 \
-#   --log logs/combined.log \
-#   --output logs/output.log \
-#   --error logs/error.log \
-#   --merge-logs \
-#   --log-date-format "YYYY-MM-DD HH:mm:ss" \
