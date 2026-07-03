ca03d6e0e9b1627b5cb0454ea5b9692c9a71ef00
NotKeKe <149957866+NotKeKe@users.noreply.github.com>
2026-07-03 15:16:25 +0800

    feat: 在 close_event 中，加上了 voice_client 被關閉的消息

--- core/close_event.py
+++ core/close_event.py
@@ -65,6 +65,15 @@ async def close_event():
     try:
         from cmds.play4 import players
         for player in players.values():
+            voice_client = player.voice_client
+            if voice_client and voice_client.is_connected(): # type: ignore
+                try:
+                    await voice_client.disconnect(force=True)
+                except Exception: 
+                    pass
+
+                await player.ctx.send(f'Bot is currently restarting, we sincerely apologize for the inconvenience. You may need to manually re-invite YinXi later.')
+            
             await player._cleanup()
         players.clear()
         logger.info('Closed players')
