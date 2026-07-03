0b86bb0125b7c7b58af925df94a5b61e813a22c0
NotKeKe <149957866+NotKeKe@users.noreply.github.com>
2026-07-03 16:50:03 +0800

    完成 handle_completion

--- cmds/ai_chat/v2/chater/main.py
+++ cmds/ai_chat/v2/chater/main.py
@@ -9,6 +9,8 @@ from ..types.chater import *
 from ..ai_model.detector import ModelDetector
 from ..data_keeper.clients import get_openai_client
 
+from .utils import get_think, clean_text
+
 class Chater:
     def __init__(
         self, 
@@ -18,7 +20,8 @@ class Chater:
         self._infos = Infos(
             meta=Meta(
                 model=ModelDetector.detect_to_model(model) if model else Model(),
-                ctx=ctx
+                ctx=ctx,
+                system_prompt=""
             ),
             history=[]
         )
@@ -28,21 +31,51 @@ class Chater:
         ...
 
     async def _handle_completion(self, response: ChatCompletion) -> CompletionResponse:
-        ...
+        if not response or not response.choices:
+            raise ValueError('AI has no response (no response or no response.choices[0])')
+
+        message = response.choices[0].message
+
+
+        if message.content:
+            result = message.content
+
+        think = ""
+        if (hasattr(message, 'reasoning_content')):
+            think = str(message.reasoning_content)
+        elif (hasattr(message, 'reasoning')):
+            think = str(message.reasoning)
+        
+        # 如果 think 是被包含在 result 裡面，到這裡才會做處理
+        # 這裡再 clean_text 是為了避免在正常情況下，刪到 LLM 原本就要輸出的東西
+        if not think:
+            think = get_think(result)
+            result = clean_text(result)
+
+        total_tokens = response.usage.total_tokens if response.usage else -1
+
+        return CompletionResponse(
+            think=think,
+            result=result.strip(),
+            tool_calls=message.tool_calls,
+            token_count=total_tokens
+        )
         
 
-    def change_system_prompt(self):
-        ...
+    def change_system_prompt(self, prompt: str):
+        self._infos.meta.system_prompt = prompt
 
     def change_model(self, model: str):
         self._model = ModelDetector.detect_to_model(model)
 
-    async def chat(self) -> ChatResponse:
+    async def chat(self, ctx: commands.Context) -> ChatResponse:
+        # 不同訊息會有不同的 commands.Context 物件
+        self._infos.meta.ctx = ctx
+        
         client = get_openai_client(self._model.provider)
 
         self._infos.history.append(
             UserMessage(
-                role='user',
                 content=f'`{self._infos.meta.ctx.author.global_name}` said: 「{self._infos.meta.ctx.message.content}」'
             )
         )
@@ -57,8 +90,12 @@ class Chater:
                 raise ValueError('AI has no response (no response.choices[0])')
 
             comp_resp = await self._handle_completion(resp)
-            if comp_resp.tool_calls:
-                await self._handle_tool_call(comp_resp.tool_calls, self._infos)
+
+            # 沒有工具調用就跳出迴圈
+            if not comp_resp.tool_calls:
+                break
+
+            await self._handle_tool_call(comp_resp.tool_calls, self._infos)
 
         return ChatResponse(
 
